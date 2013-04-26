0b16d8885225d31bfaeed6885a473bf2dd277e77
Cheeseen <aychtang@gmail.com>
2013-04-26 18:42:19 +0100

    only exposes one function to window, makes second param optional

--- canvas.js
+++ canvas.js
@@ -1,21 +1,15 @@
 
 (function() {
+	console.log('The console method is size(padding, size); If the second parameter isn\'t passed in, it will assume the size of the first.')
 	var canvas = document.createElement("canvas");
 	var context = canvas.getContext("2d");
 	canvas.width = document.width;
 	canvas.height = document.height;
 	document.body.appendChild(canvas);
 
-	window.square = {
+	var square = {
 		pad: 10,
 		box: 10,
-
-		size : function(padding, size) {
-			if (padding === 0) return 'Please don\'t do that';
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			square.box = size;
-			square.pad = padding;
-		},
 
 		getPadding : function() {
 			return square.pad;
@@ -26,11 +20,18 @@
 		}
 	};
 
+	window.size = function(padding, size) {
+		if (padding === 0) return 'Please don\'t do that';
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		square.box = size || padding;
+		square.pad = padding;
+	};
+
 	var draw = function() {
 	  for (var i = 0; i < canvas.height / square.getPadding(); i++) {
 	    for (var j = 0; j < canvas.width / square.getPadding(); j++) {
 	      context.fillStyle = 'rgb(' + ~~(Math.random() * 255) + ',' + ~~(Math.random() * 255) + ',' + ~~(Math.random() * 255) + ')';
-	      context.fillRect( j * square.getPadding(), i * square.getPadding(), square.getSize(), square.getSize());
+	      context.fillRect(j * square.getPadding(), i * square.getPadding(), square.getSize(), square.getSize());
 	    }
 	  }
 	};
