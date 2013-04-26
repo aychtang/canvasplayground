aed320f75fcc1b08a9978a3e171b6a3e16505688
Cheeseen <aychtang@gmail.com>
2013-04-26 17:57:46 +0100

    adds a console interface square.size(n), which allows you to set the size in real time

--- canvas.js
+++ canvas.js
@@ -1,17 +1,33 @@
 
-var canvas = document.createElement("canvas");
-var context = canvas.getContext("2d");
-canvas.width = document.width;
-canvas.height = document.height;
-document.body.appendChild(canvas);
+(function() {
+	var canvas = document.createElement("canvas");
+	var context = canvas.getContext("2d");
+	canvas.width = document.width;
+	canvas.height = document.height;
+	document.body.appendChild(canvas);
 
-var draw = function() {
-  for (var i = 0; i < canvas.height / 10; i++) {
-    for(var j = 0; j < canvas.width / 10; j++) {
-      context.fillStyle = 'rgb(' + ~~(Math.random() * 255) + ',' + ~~(Math.random() * 255) + ',' + ~~(Math.random() * 255) + ')';
-      context.fillRect( j * 10, i * 10,10,10);
-    }
-  }
-};
+	window.square = {
+		box: 10,
 
-setInterval(draw, 17);
+		size : function(size) {
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			square.box = size;
+		},
+
+		get: function() {
+			return square.box;
+		}
+	};
+
+	var draw = function() {
+	  for (var i = 0; i < canvas.height / square.get(); i++) {
+	    for (var j = 0; j < canvas.width / square.get(); j++) {
+	      context.fillStyle = 'rgb(' + ~~(Math.random() * 255) + ',' + ~~(Math.random() * 255) + ',' + ~~(Math.random() * 255) + ')';
+	      context.fillRect( j * square.get(), i * square.get(), square.get(), square.get());
+	    }
+	  }
+	};
+
+	setInterval(draw, 17);
+
+}())
